39a33e5850e231965ec98ec542ccd33c2eef18a8
Vitthal Bhandari <vitthalbhandari98@gmail.com>
2020-09-20 11:57:21 +0530

    mern-skeleton

---
+++ client/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>I am Vitthal Bhandari!</h1>
+        </div>
+    )
+}
+
+export default hot(module) (HelloWorld)
